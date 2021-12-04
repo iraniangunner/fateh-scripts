ddbd5f583637735485325a09b5194f0a937c7fe2
iraniangunner <53290111+iraniangunner@users.noreply.github.com>
2021-12-05 03:16:31 +0330

    Create getRepeatedInfo.js

---
+++ getRepeatedInfo.js
@@ -0,0 +1,31 @@
+export function getRepeatedInfo(number) {
+  const convertedNumber = number.toString();
+  const arrayOfDigits = convertedNumber.split("");
+
+  let result = null;
+
+  result = {};
+
+  for (let i = 0; i < arrayOfDigits.length; i++) {
+    if (Object.keys(result).includes(arrayOfDigits[i])) {
+      continue;
+    }
+    let count = 0;
+    for (let j = i; j < arrayOfDigits.length; j++) {
+      if (arrayOfDigits[i] === arrayOfDigits[j]) {
+        count++;
+      }
+    }
+
+    let arr = [];
+
+    for (let k = 0; k < arrayOfDigits[i] * count; k++) {
+      arr.push(arrayOfDigits[i]);
+    }
+    const myResult = arr.join("");
+
+    result[Number(arrayOfDigits[i])] = myResult;
+  }
+
+  return result;
+}
